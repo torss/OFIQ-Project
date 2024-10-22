b212c1205a00cb082f55909bbbe91b5adf7785c4
torss <37229901+torss@users.noreply.github.com>
2024-10-22 16:05:26 +0200

    Update build__only_build.sh re. "macos" build_dir selection

--- scripts/build__only_build.sh
+++ scripts/build__only_build.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-# Just the cmake --build part from build.sh.
+# Just the main OFIQ "cmake --build" part from build.sh.
 
 build_dir=build/build_linux
+
+while [ -n "$1" ]
+do  
+if [ "$1" = "--os" ]; then
+    shift
+    if [ "$1" = "macos" ]; then
+        build_dir=build/build_mac
+    else
+        echo "$1" is a not a supported OS
+        exit
+    fi
+else
+    echo "$1" is not a supported argument
+    exit
+fi
+shift
+done
+
 cd ../
 cmake --build $build_dir --target install -j 8
 
