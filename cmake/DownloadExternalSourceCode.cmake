73fed6771856fdb575f545a7520ea0becc191088
tams.benjamin <Benjamin.Tams@secunet.com>
2025-07-25 10:33:50 +0200

    cleans up building files

--- cmake/DownloadExternalSourceCode.cmake
+++ cmake/DownloadExternalSourceCode.cmake
@@ -1,8 +1,11 @@
 set(FILE OFIQ-EXTERN.zip)
-set(URL https://standards.iso.org/iso-iec/29794/-5/ed-1/en/OFIQ-EXTERN.zip)
 
-message("Downloading external source code from ${URL}")
-file(DOWNLOAD ${URL} ${FILE} SHOW_PROGRESS)
+if (NOT DEFINED OFIQ_EXTERN_URL)
+	set(OFIQ_EXTERN_URL https://standards.iso.org/iso-iec/29794/-5/ed-1/en/OFIQ-EXTERN.zip)
+endif()
+
+message("Downloading external source code from ${OFIQ_EXTERN_URL}")
+file(DOWNLOAD ${OFIQ_EXTERN_URL} ${FILE} SHOW_PROGRESS)
 message("Extracting external source code")
 file(ARCHIVE_EXTRACT 
     INPUT ${FILE}
